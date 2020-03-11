74ebb87dd2871739629cefd9ca847c5bb1f1927f
smk-yodlee <56870265+smk-yodlee@users.noreply.github.com>
2020-03-11 08:56:22 +0530

    Create Extra_long_factorials.c

---
+++ Extra_long_factorials.c
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+//For N.20, factorials cant be stored in 64 bit integers
+//Create array of 500 or so size
+//Store every digit of a number in a different index of the array
+//At every step, calculate k*a[index]
+//Add carry if needed
+//Get new values for all a[index] and then continue the process
+int main() {
+    int val;
+    int carry = 0;
+    cin >> val;
+    vector <int> arr(200, 0);
+    arr[0] = 1; //Initial product = 1
+
+    int k = 0; //Current size of the number stored in arr
+
+    for(int i = 1; i <= val; i++) {
+        for(int j = 0;j <= k; j++) {
+            arr[j] = arr[j] * i + carry;
+            carry = arr[j] / 10;
+            arr[j] = arr[j] % 10;
+        }
+        while(carry) { //Propogate the remaining carry to higher order digits
+            k++;
+            arr[k] = carry % 10;
+            carry /= 10;
+        }   
+    }
+    for(int i = k; i >= 0; i--) {
+        cout << arr[i];
+    }
+    cout << endl;
+    return 0;
+}
